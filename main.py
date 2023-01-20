2daf24ca7a87dc5317efd6294a698f82b6041ab7
Gowtham Krishna <gowtham.krishna@live.com>
2023-01-20 11:04:10 +0530

    new

--- main.py
+++ main.py
@@ -2,23 +2,38 @@ BACKGROUND_COLOR = "#B1DDC6"
 import tkinter
 import pandas
 import random
-
-data = pandas.read_csv('data/french_words.csv')
-new_word = data.to_dict(orient='records')
 current_card={}
+to_learn = {}
+
+try:
+    data = pandas.read_csv('data/words to learn.csv')
+except FileNotFoundError:
+    orginal_data = pandas.read_csv('data/french_words.csv')
+    to_learn = orginal_data.to_dict(orient='records')
+else:
+    words = data.to_dict(orient='records')
+
 def next_card():
     global current_card, timer_flip
     window.after_cancel(timer_flip)
-    current_card= random.choice(new_word)
+    current_card= random.choice(words)
     canvas.itemconfig(language, text='French', fill='black')
     canvas.itemconfig(word, text=current_card['French'], fill='black')
     canvas.itemconfig(background, image= card_front_image)
     timer_flip = window.after(3500, func=flip_card)
 
+
 def flip_card():
     canvas.itemconfig(language, text='English', fill='white')
     canvas.itemconfig(word, text=current_card['English'], fill='white')
     canvas.itemconfig(background, image= card_back_image)
+
+def is_known():
+    words.remove(current_card)
+    words_left = pandas.DataFrame(words)
+    words_left.to_csv('data/words to learn.csv', index= False)
+    next_card()
+
 
 window = tkinter.Tk()
 window.title('Flashy')
@@ -38,7 +53,7 @@ right_img = tkinter.PhotoImage(file='images/right.png')
 right_btn = tkinter.Button(image=right_img, highlightthickness=0,command=next_card)
 right_btn.grid(column=0, row= 1)
 left_img = tkinter.PhotoImage(file='images/wrong.png')
-wrong_btn = tkinter.Button(image=left_img, highlightthickness=0,command=next_card)
+wrong_btn = tkinter.Button(image=left_img, highlightthickness=0,command=is_known)
 wrong_btn.grid(column=1, row= 1)
 
 next_card()
